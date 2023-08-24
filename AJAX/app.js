0e3b3fc35f5a89aa6a6c5c47eed1fc3a1a6590f0
FranChiz28 <fcssalvador28@gmail.com>
2023-08-24 21:11:11 +0800

    AJAX

--- AJAX/app.js
+++ AJAX/app.js
@@ -3,20 +3,23 @@ function getRequest() {
 
     
     xhr.onload = function(){
+      if (this.status == 200) {
       const res = JSON.parse(xhr.responseText);
-      let resPage = document.getElementById('js_output').innerHTML;
+      let output;
   
-      output = '<div>' + '<img src="' + res[0].url + '" width="200" height="200"><br>' + 'ID:' + res[0].id + '</div>'
+      for (let i in res){
+      output += '<div>' + '<img src="' + res[i].url + '" width="200" height="200"><br>' + 'ID:' + res[i].id + '</div>'
       document.getElementById('js_output').innerHTML = output;
     
-
+      }
     }
+  }
 
     const apiKey = 'api_key=live_VcbaqAGCyho52KWAwPZBykvv8WOfzcLZhw45djBMdc2HKLAuvwIMlP5FhF3UGozV';
-    const endpoint = 'https://api.thedogapi.com/v1/images/search';
+    const endpoint = 'https://api.thedogapi.com/v1/images/search?limit=10';
     const dogApi = endpoint + '?' + apiKey;
 
-    xhr.open("GET", dogApi, true);
+    xhr.open("GET", endpoint, true);
     
     xhr.send();
   }
