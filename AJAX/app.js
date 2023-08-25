7b3060c4bc0d229909800d24b243fecb34f9f55d
FranChiz28 <fcssalvador28@gmail.com>
2023-08-25 20:36:47 +0800

    AJAX

--- AJAX/app.js
+++ AJAX/app.js
@@ -5,18 +5,18 @@ function getRequest() {
     xhr.onload = function(){
       if (this.status == 200) {
       const res = JSON.parse(xhr.responseText);
-
+      let output;
       for (let i in res){
-        let output;
         output += '<div>' + '<img src="' + res[i].url + '" width="200" height="200"><br>' + 'ID:' + res[i].id + '</div>'
       document.getElementById('js_output').innerHTML = output;
-      }
+      console.log(res[i]);
+    }
     }
   }
 
     const apiKey = 'api_key=live_VcbaqAGCyho52KWAwPZBykvv8WOfzcLZhw45djBMdc2HKLAuvwIMlP5FhF3UGozV';
     const endpoint = 'https://api.thedogapi.com/v1/images/search?limit=10';
-    const dogApi = endpoint + '?' + apiKey;
+    const dogApi = endpoint + apiKey;
 
     xhr.open("GET", endpoint, true);
     
